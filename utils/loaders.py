02f489088e0f80c56c15f013b258f7ccad1abf25
GianlucaCarlini <gianluca.carlini@outlook.it>
2025-01-28 14:48:51 +0100

    Initializing a repository for SSL with MoBy and 3D swin transformer

---
+++ utils/loaders.py
@@ -0,0 +1,312 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import SimpleITK as sitk
+import os
+from typing import Union, Callable, Any
+import os
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+__all__ = ["PatchDataset", "ImageDataset"]
+
+
+def gaussian_sampling(volume_size: tuple, patch_size: tuple, std_factor: int = 8):
+    """Sample a patch using a gaussian distribution centered at the center of the volume.
+
+    Parameters
+    ----------
+    volume_size : tuple
+        The size of the volume in the x, y, and z dimensions.
+    patch_size : tuple
+        The size of the patch to sample in the x, y, and z dimensions.
+    std_factor : int, optional
+        Controls the spreading of the gaussian. The standard deviation to use is
+        calculated as the volume size divided by this factor; so the smaller the factor,
+        the more spread out the gaussian. Defaults to 8.
+
+    Returns
+    -------
+    tuple
+        The x, y, and z indices of the sampled patch.
+    """
+    if patch_size[0] < 0:
+        return 0, 0, 0
+
+    x, y, z = volume_size
+
+    x_offset = patch_size[0] / 2
+    y_offset = patch_size[1] / 2
+    z_offset = patch_size[2] / 2
+
+    center_x = x / 2 - x_offset
+    center_y = y / 2 - y_offset
+    center_z = z / 2 - z_offset
+
+    std_x = x / std_factor
+    std_y = y / std_factor
+    std_z = z / std_factor
+
+    extract_idx_x = np.random.normal(loc=center_x, scale=std_x)
+    extract_idx_y = np.random.normal(loc=center_y, scale=std_y)
+    extract_idx_z = np.random.normal(loc=center_z, scale=std_z)
+
+    extract_idx_x = int(np.clip(extract_idx_x, 0, x - patch_size[0]))
+    extract_idx_y = int(np.clip(extract_idx_y, 0, y - patch_size[1]))
+    extract_idx_z = int(np.clip(extract_idx_z, 0, z - patch_size[2]))
+
+    return extract_idx_x, extract_idx_y, extract_idx_z
+
+
+def uniform_sampling(volume_size: tuple, patch_size: tuple):
+    """Sample a patch using a uniform distribution.
+
+    Parameters
+    ----------
+    volume_size : tuple
+        The size of the volume in the x, y, and z dimensions.
+    patch_size : tuple
+        The size of the patch to sample in the x, y, and z dimensions.
+
+    Returns
+    -------
+    tuple
+        The x, y, and z indices of the sampled patch.
+    """
+    if patch_size[0] < 0:
+        return 0, 0, 0
+
+    x, y, z = volume_size
+
+    if patch_size is None:
+        patch_size = (0, 0, 0)
+
+    extract_idx_x = np.random.randint(0, max(x - patch_size[0], 1))
+    extract_idx_y = np.random.randint(0, max(y - patch_size[1], 1))
+    extract_idx_z = np.random.randint(0, max(z - patch_size[2], 1))
+
+    return extract_idx_x, extract_idx_y, extract_idx_z
+
+
+sampling_functions = {"uniform": uniform_sampling, "gaussian": gaussian_sampling}
+
+
+class PatchDataset(Dataset):
+    """
+    Dataset for lazy loading patches from images and labels.
+    It samples a random index from the image and label and
+    extracts a patch of the specified size.
+    If the number of non-zero voxels in the label is less than the
+    threshold, it will sample another index until the threshold is met.
+
+    Attributes
+    ----------
+    images_dir : str
+        path to the image directory
+    labels_dir : str
+        path to the label directory
+    patch_size : tuple | int, optional
+        Size of the patches to load as a tuple of ints
+        representing the x, y, and z dimension. If a single int is provided,
+        the same value will be used for x, y, and z.
+        If less than 0, the whole volume is used. Defaults to None.
+    sampling_method : str, optional
+        Sampling method to use. Can be either
+        "uniform" or "gaussian". Defaults to "uniform".
+    threshold : float, optional
+        Threshold value to consider for patch sampling.
+        If the sum of non-zero pixels in the sampled patch is lower than
+        threshold, then another patch is sampled until the threshold condition is met
+        Defaults to None.
+    transform : callable, optional
+        Optional transform to apply to the image and label.
+        The same transform is applied to both. Defaults to None.
+    preprocessing : callable, optional
+        Optional preprocessing to apply to the image.
+        Defaults to None.
+
+    Methods
+    -------
+    __getitem__(index)
+        Returns the image and label at the specified index.
+    __len__()
+        Returns the length of the dataset.
+    get_relative_index()
+        Returns the relative index of the sampled patch. By relative index
+        we mean the index of the patch divided by the size of the volume, so that
+        the index is in the range [0, 1], and different volumes are comparable.
+
+    """
+
+    def __init__(
+        self,
+        images_dir: str,
+        labels_dir: str = None,
+        patch_size: Union[tuple, int] = None,
+        sampling_method: str = "uniform",
+        threshold: float = None,
+        transform: Callable = None,
+        preprocessing: Callable = None,
+        positional: bool = False,
+        repeat: int = 1,
+        **kwargs,
+    ):
+        self.images_dir = images_dir
+        self.labels_dir = labels_dir
+
+        self.ids = os.listdir(self.images_dir)
+
+        if repeat > 1:
+            self.ids = self.ids * repeat
+
+        self.images = [os.path.join(self.images_dir, image_id) for image_id in self.ids]
+
+        if self.labels_dir is not None:
+            self.labels = [
+                os.path.join(self.labels_dir, image_id) for image_id in self.ids
+            ]
+        else:
+            self.labels = self.images
+
+        if preprocessing is not None:
+            self.preprocessing = preprocessing
+        else:
+            self.preprocessing = None
+
+        if transform is not None:
+            self.transform = transform
+        else:
+            self.transform = None
+
+        self.positional = positional
+
+        self.reader = sitk.ImageFileReader()
+
+        self.sampling_method = sampling_method
+
+        self.sampling_function = sampling_functions.get(self.sampling_method, None)
+
+        if self.sampling_function is None:
+            raise NotImplementedError(
+                f"Sampling method {self.sampling_method} not implemented, available methods are: {sampling_functions.keys()}"
+            )
+
+        if patch_size is not None:
+            if isinstance(patch_size, int):
+                self.patch_size = (patch_size, patch_size, patch_size)
+            else:
+                self.patch_size = patch_size
+        else:
+            self.patch_size = (96, 96, 96)
+
+        if threshold is not None:
+            self.threshold = threshold
+        else:
+            self.threshold = 0.0
+
+        self.len = len(self.ids)
+
+        self.kwargs = kwargs
+
+    def __getitem__(self, index):
+        self.reader.SetFileName(self.labels[index])
+        self.reader.ReadImageInformation()
+
+        self.x, self.y, self.z = self.reader.GetSize()
+
+        if self.patch_size[0] < 0:
+            patch_size = (self.x, self.y, self.z)
+        else:
+            patch_size = self.patch_size
+
+        while True:
+            (
+                self.extract_idx_x,
+                self.extract_idx_y,
+                self.extract_idx_z,
+            ) = self.sampling_function(
+                volume_size=(self.x, self.y, self.z),
+                patch_size=patch_size,
+                **self.kwargs,
+            )
+
+            self.reader.SetExtractIndex(
+                (self.extract_idx_x, self.extract_idx_y, self.extract_idx_z)
+            )
+            self.reader.SetExtractSize((patch_size[0], patch_size[1], patch_size[2]))
+
+            label = self.reader.Execute()
+            label = sitk.GetArrayFromImage(label)
+
+            label = np.abs(label)
+
+            if np.sum(label > 0.0) > self.threshold * (
+                patch_size[0] * patch_size[1] * patch_size[2]
+            ):
+                break
+
+        self.reader.SetFileName(self.images[index])
+
+        image = self.reader.Execute()
+
+        if self.preprocessing is not None:
+            image = self.preprocessing(image)
+
+        image = sitk.GetArrayFromImage(image)
+
+        image = np.expand_dims(image, axis=0)
+
+        if self.transform is not None:
+            image, label = self.transform(image, label)
+
+        image = torch.from_numpy(image).float()
+        label = torch.from_numpy(label).float()
+
+        if self.positional:
+            rel_idx = self.get_relative_index()
+            rel_idx = torch.from_numpy(np.array(rel_idx)).float()
+
+            return image, label, rel_idx
+
+        if self.labels_dir is None:
+            return image
+
+        return image, label
+
+    def __len__(self):
+        return self.len
+
+    def get_relative_index(self):
+        rel_x = self.extract_idx_x / self.x
+        rel_y = self.extract_idx_y / self.y
+        rel_z = self.extract_idx_z / self.z
+
+        return [[rel_z], [rel_y], [rel_x]]
+
+
+class ImageDataset(Dataset):
+    def __init__(self, image_dir, transform=None, pre_processing=None):
+        self.image_dir = image_dir
+        self.transform = transform
+        self.pre_processing = pre_processing
+
+        self.image_ids = os.listdir(image_dir)
+
+        self.images = [os.path.join(image_dir, image_id) for image_id in self.image_ids]
+
+        self.images = self.images
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+
+        image = PIL.Image.open(self.images[idx]).convert("RGB")
+
+        if self.pre_processing:
+            image = self.pre_processing(image)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image
---
+++ utils/ssl_ops.py
@@ -0,0 +1,245 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from numpy.random import randint
+
+
+def corrupt_image(
+    image: torch.Tensor,
+    patch_size: int = 32,
+    frac: float = 0.2,
+    mode: str = "disc",
+    retrun_proportion: bool = False,
+) -> torch.Tensor:
+    """
+    corrupt an image by modifying patches of a given size.
+    It expects the image to be in the format (batch, channels, height, width, (depth)).
+    The percentage of patches to be shuffled is given by frac.
+    Note that the image dimensions should be exactly divisible by the patch_size;
+    I don't know the behavior of the function otherwise.
+    This function should work for both 2D and 3D images. In the case of 3D images,
+    a single channel is assumed.
+
+    Parameters
+    ----------
+    image : torch.Tensor
+        Image to be corrupted.
+    patch_size : int, optional
+        Size of the patches to corrupt, by default 32
+    frac : float, optional
+        Percentage of patches to corrupt, by default 0.2
+    mode : str, optional
+        Mode of corruption, either "disc" or "mask", by default "disc".
+        If "disc", the patches will be shuffled. If "mask", the patches will be zeroed-out.
+        Disc is short for discombobulate, which is a fun word.
+    retrun_proportion : bool, optional
+        If True, the proportion of patches that were shuffled will be returned.
+        More in detail, it will return a matrix nxn where n is the number of images in the batch.
+        The (i, j) element of the matrix represents the proportion of patches of the image j
+        that were shuffled to image i (i being the rows and j the columns of the matrix).
+        By default False. Has no effect if mode is "mask".
+
+    Returns
+    -------
+    corrupted : torch.Tensor
+        corrupted image.
+    unique : torch.Tensor
+        If return_proportion is True, returns the matrix containing the
+        proportion of patches of tensor j in tensor i.
+
+    Raises
+    ------
+    ValueError
+        If the image is not 4D or 5D
+    ValueError
+        If the image is 5D and has more than one channel
+    ValueError
+        If the mode is not "disc" or "mask"
+
+    Examples
+    --------
+    >>> img_tensor = torch.randn(1, 3, 256, 256)
+    >>> disc_img = corrupt_image(img_tensor, patch_size=64, frac=0.25, mode="disc")
+    >>> disc_img.shape
+    """
+
+    image_ndim = image.ndim
+
+    if image_ndim == 4:
+        channels = image.shape[1]
+        first_dim_size = channels
+    elif image_ndim == 5:
+
+        if image.shape[1] != 1:
+            raise ValueError("Only single-channel images are supported for 5D tensors")
+
+        image = image.squeeze(1)  # remove channel dimension
+        first_dim_size = patch_size
+    else:
+        raise ValueError(f"Only 4D and 5D tensors are supported, not {image_ndim}D")
+
+    patches = (
+        image.unfold(1, first_dim_size, first_dim_size)
+        .unfold(2, patch_size, patch_size)
+        .unfold(3, patch_size, patch_size)
+    )
+    unfold_shape = patches.size()
+    patches = patches.contiguous().view(1, -1, first_dim_size, patch_size, patch_size)
+
+    idxs = np.arange(patches.shape[1])
+
+    shuffled_idxs = idxs.copy()
+    np.random.shuffle(shuffled_idxs)
+
+    # shuffled_pos = pos[shuffled_idxs]
+
+    shuffled_idxs = shuffled_idxs[: int(len(shuffled_idxs) * frac)]
+    _shuffle_idxs = shuffled_idxs.copy()
+    np.random.shuffle(_shuffle_idxs)
+
+    idxs[shuffled_idxs] = _shuffle_idxs
+
+    n_patches = patches.shape[1] // image.shape[0]
+
+    # TODO: probably this can be made more efficient
+    # like: patches[:, shuffled_idxs, ...] = patches[:, _shuffle_idxs, ...]
+    # I'll test it later
+    if mode == "disc":
+        patches[:, shuffled_idxs, ...] = patches[:, _shuffle_idxs, ...]
+    elif mode == "mask":
+        patches[:, _shuffle_idxs, ...] = 0
+    else:
+        raise ValueError('Mode should be either "disc" or "mask"')
+
+    corrupted = patches.view(unfold_shape)
+    corrupted = corrupted.permute(0, 1, 4, 2, 5, 3, 6).contiguous()
+    corrupted = corrupted.view(image.shape)
+
+    if image_ndim == 5:
+        corrupted = corrupted.unsqueeze(1)  # add channel dimension back
+
+    if retrun_proportion and mode == "disc":
+        # create a vector associating each patch with its image
+        pos = torch.arange(image.shape[0])
+
+        rep_pos = pos.repeat(image.shape[0])
+
+        pos = pos.reshape(image.shape[0], 1)
+        rep_pos_ = torch.repeat_interleave(pos, image.shape[0], dim=1)
+        rep_pos_ = torch.flatten(rep_pos_)
+
+        # find all the possible combinations
+        all_combinations = torch.cat([rep_pos_[:, None], rep_pos[:, None]], dim=1)
+
+        pos = torch.repeat_interleave(pos, n_patches, dim=1)
+        pos = torch.flatten(pos)
+
+        # shuffle the pos vector according to the shuffled indexes
+        shuffled_pos = pos[shuffled_idxs]
+        _shuffled_pos = pos[_shuffle_idxs]
+
+        # combine the shuffled positions and count the unique combinations
+        combination = torch.cat([shuffled_pos[:, None], _shuffled_pos[:, None]], dim=1)
+
+        all_combinations_exp = all_combinations.unsqueeze(1).expand(
+            -1, combination.shape[0], 2
+        )
+        combination_exp = combination.unsqueeze(0).expand(
+            all_combinations.shape[0], -1, 2
+        )
+
+        # find which combinations do not occur, i.e., which tensors j have
+        # no patches shuffled to tensor i
+        mask = (all_combinations_exp == combination_exp).all(-1).any(-1)
+
+        # count how many times each combination occurs
+        _, unique = torch.unique(combination, dim=0, return_counts=True)
+
+        # put the number of unique values in the right places
+        # non-existing combinations will be filled with zeros
+        zeros = torch.zeros(len(mask), dtype=torch.int64)
+        zeros[torch.where(mask)] = unique
+
+        unique = zeros.reshape(image.shape[0], -1)
+        unique = unique / n_patches
+        unique.fill_diagonal_(0)
+        add = torch.diag(1 - (unique.sum(dim=1)))
+        unique = unique + add
+        unique.to(image.device)
+        corrupted.to(image.device)
+
+        return corrupted, unique
+
+    return corrupted
+
+
+class CorruptImage(nn.Module):
+
+    def __init__(
+        self,
+        patch_size: int = 32,
+        frac: float = 0.2,
+        mode: str = "disc",
+        return_proportion: bool = False,
+    ):
+        """
+        corrupt an image by modifying patches of a given size.
+        It expects the image to be in the format (batch, channels, height, width, (depth)).
+        The percentage of patches to be shuffled is given by frac.
+        Note that the image dimensions should be exactly divisible by the patch_size;
+        I don't know the behavior of the function otherwise.
+        This function should work for both 2D and 3D images. In the case of 3D images,
+        a single channel is assumed.
+
+        Parameters
+        ----------
+        image : torch.Tensor
+            Image to be corrupted.
+        patch_size : int, optional
+            Size of the patches to corrupt, by default 32
+        frac : float, optional
+            Percentage of patches to corrupt, by default 0.2
+        mode : str, optional
+            Mode of corruption, either "disc" or "mask", by default "disc".
+            If "disc", the patches will be shuffled. If "mask", the patches will be zeroed-out.
+            Disc is short for discombobulate, which is a fun word.
+        retrun_proportion : bool, optional
+            If True, the proportion of patches that were shuffled will be returned.
+            More in detail, it will return a matrix nxn where n is the number of images in the batch.
+            The (i, j) element of the matrix represents the proportion of patches of the image j
+            that were shuffled to image i (i being the rows and j the columns of the matrix).
+            By default False. Has no effect if mode is "mask".
+
+        Returns
+        -------
+        corrupted : torch.Tensor
+            corrupted image.
+        unique : torch.Tensor
+            If return_proportion is True, returns the matrix containing the
+            proportion of patches of tensor j in tensor i.
+
+        Raises
+        ------
+        ValueError
+            If the image is not 4D or 5D
+        ValueError
+            If the image is 5D and has more than one channel
+        ValueError
+            If the mode is not "disc" or "mask"
+
+        Examples
+        --------
+        >>> img_tensor = torch.randn(1, 3, 256, 256)
+        >>> disc_img = corrupt_image(img_tensor, patch_size=64, frac=0.25, mode="disc")
+        >>> disc_img.shape
+        """
+        super(CorruptImage, self).__init__()
+        self.patch_size = patch_size
+        self.frac = frac
+        self.mode = mode
+        self.return_proportion = return_proportion
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        return corrupt_image(
+            image, self.patch_size, self.frac, self.mode, self.return_proportion
+        )
